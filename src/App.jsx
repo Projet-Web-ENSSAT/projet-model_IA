cb83edf663889d6db37f05cab01062bf0ccb8147
emclugery <emma@gmail.com>
2026-04-21 16:26:01 +0200

    Belle page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import Scene from "./Scene";
 import "./App.css";
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
   return (
     <div className="App">
+      {showIntro && (
+        <div className="block">
+          <h1 className="title">Bienvenue sur Astro-Learning</h1>
+          <p className="description">
+            Découvrez les merveilles de l’univers grâce à notre modèle interactif du système solaire en 3D. Explorez les planètes, les lunes et les corps célestes avec un niveau de détail remarquable, et apprenez leurs caractéristiques uniques ainsi que des faits fascinants. Que vous soyez étudiant, enseignant ou passionné d’espace, Astro-Learning offre une expérience immersive et éducative pour tous les âges. Commencez votre voyage cosmique dès aujourd’hui et découvrez les mystères de l’univers !
+          </p>
+          <button 
+            className="button"
+            onClick={() => setShowIntro(false)}
+          >
+            Explore maintenant 
+          </button>
+        </div>
+      )}
+
       <Scene />
     </div>
   );
