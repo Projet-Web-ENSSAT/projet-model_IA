c53fae9e32b2681dcd0117bef7e8a726ee9ed740
emclugery <emma@gmail.com>
2026-04-22 10:41:28 +0200

    font

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
             className="button"
             onClick={() => setShowIntro(false)}
           >
-            Explore maintenant 
+            Explorez maintenant 
           </button>
         </div>
       )}
